e442016635ab45a101db74d70ad78c00e75f6241
nathaniel121215 <nathaniel121215@gmail.com>
2023-07-12 12:09:20 +0800

    backend fix

--- pen-tech-react/src/components/Contactus.jsx
+++ pen-tech-react/src/components/Contactus.jsx
@@ -60,7 +60,7 @@ const Contactus = ({mode}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:5000/send-email', email)
+    axios.post('https://pen-tech-website-voqn.vercel.app/send-email', email)
       .then(response => {
         console.log(response.data);
         setEmail({Customer_Name: '', 
