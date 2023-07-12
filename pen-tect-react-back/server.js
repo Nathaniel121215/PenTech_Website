78e88064289a0efce85c59c8b5c4e593929b33f3
nathaniel121215 <nathaniel121215@gmail.com>
2023-07-12 13:33:57 +0800

    backend update 14

--- pen-tect-react-back/server.js
+++ pen-tect-react-back/server.js
@@ -46,6 +46,6 @@ app.post('/api/send-email', (req, res) => {
   });
 });
 
-// app.listen(5000, () => {
-//   console.log('Server started on port 5000');
-// });
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
