195b241d9de357abb82c76f0ca9b4d55aa32dfd9
nathaniel121215 <nathaniel121215@gmail.com>
2023-07-13 20:54:30 +0800

    contact us update

--- pen-tech-react/src/components/Contactus.jsx
+++ pen-tech-react/src/components/Contactus.jsx
@@ -60,7 +60,7 @@ const Contactus = ({mode}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('/send-email', email)
+    axios.post('https://pen-tech-solution.netlify.app//send-email', email)
       .then(response => {
         console.log(response.data);
         setEmail({Customer_Name: '', 
